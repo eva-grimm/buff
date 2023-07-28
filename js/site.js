8d23ecb7062c9ebdb7b4eee4032f70179bfb5ff1
Cadence Eva Custin <eva.custin@gmail.com>
2023-07-28 12:45:05 -0400

    - writeup for code at present done - TXT file of writeup for easier editing

--- js/site.js
+++ js/site.js
@@ -8,7 +8,8 @@ async function getMovies() {
             }
         });
         let data = await response.json();
-        return data;
+        let movies = await data.results;
+        return movies;
     } catch (error) {
         console.error(error);
     }
@@ -16,14 +17,13 @@ async function getMovies() {
 
 async function displayMovies() {
     const movieListDiv = document.getElementById('movie-list');
-    const moviePosterTemplate = document.getElementById('movie-card-template');
+    const movieCardTemplate = document.getElementById('movie-card-template');
 
     // movies is an array of objects
-    let data = await getMovies();
-    let movies = await data.results;
+    let movies = await getMovies();
 
     movies.forEach(movie => {
-        let movieCard = moviePosterTemplate.content.cloneNode(true);
+        let movieCard = movieCardTemplate.content.cloneNode(true);
 
         let title = movieCard.querySelector('.card-body > h5');
         title.textContent = movie.title;
@@ -41,11 +41,8 @@ async function displayMovies() {
     });
 }
 
-async function showMovieDetails(btn) {
-    const youtubeTemplate = document.getElementById('youtube-video');
-    let movieId = btn.getAttribute('data-movie-id')
-    let movieModal = document.getElementById('movie-modal')
-
+async function getMovie(movieId) {
+    let movie = {}
     try {
         // details
         let detailsResponse = await fetch(`https://api.themoviedb.org/3/movie/${movieId}`, {
@@ -53,7 +50,7 @@ async function showMovieDetails(btn) {
                 'Authorization': `Bearer ${API_KEY}`
             }
         });
-        let movie = await detailsResponse.json();
+        movie.details = await detailsResponse.json();
 
         // credits (cast & crew)
         let creditsResponse = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/credits`, {
@@ -61,92 +58,95 @@ async function showMovieDetails(btn) {
                 'Authorization': `Bearer ${API_KEY}`
             }
         });
-        let movieCredits = await creditsResponse.json();
+        movie.credits = await creditsResponse.json();
 
         // trailer ID
-        let trailerIdResponse = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos`, {
+        let trailerResponse = await fetch(`https://api.themoviedb.org/3/movie/${movieId}/videos`, {
             headers: {
                 'Authorization': `Bearer ${API_KEY}`
             }
         });
-        let movieTrailer = await trailerIdResponse.json();
-
-        let title = movieModal.querySelector('.modal-title');
-        title.textContent = movie.title;
-
-        // left column
-        let poster = movieModal.querySelector('img');
-        poster.setAttribute('src', `https://image.tmdb.org/t/p/w500${movie.poster_path}`);
-
-        // middle column
-        let tagline = movieModal.querySelector('.movie-tagline');
-        tagline.textContent = movie.tagline;
-
-        let director = movieModal.querySelector('.movie-director');
-        let directorHtml = ''
-        for (let i = 0; i < movieCredits.crew.length; i++) {
-            movieCredits.crew[i].job == 'Director' ? directorHtml += `${movieCredits.crew[i].name}<br>` : '';
-        }
-        director.innerHTML = directorHtml;
-
-        let writer = movieModal.querySelector('.movie-writer');
-        let writerHtml = ''
-        for (let i = 0; i < movieCredits.crew.length; i++) {
-            movieCredits.crew[i].job == 'Screenplay' || movieCredits.crew[i].job == 'Writer' ? writerHtml += `${movieCredits.crew[i].name}<br>` : '';
-        }
-        writer.innerHTML = writerHtml;
-
-        let stars = movieModal.querySelector('.movie-stars');
-        let starsHtml = '';
-        for (let i = 0; i < 4; i++) {
-            starsHtml += `${movieCredits.cast[i].name}, ${movieCredits.cast[i].character}<br>`
-        }
-        stars.innerHTML = starsHtml;
-
-        let genres = movieModal.querySelector('.movie-genres');
-        let genresHtml = '';
-        for (let i = 0; i < movie.genres.length; i++) {
-            genresHtml += `${movie.genres[i].name}<br>`
-        }
-        genres.innerHTML = genresHtml;
-
-        // right column
-        let desc = movieModal.querySelector('.movie-description');
-        desc.textContent = movie.overview;
-
-        let trailer = movieModal.querySelector('.movie-trailer');
-        trailer.innerHTML = ''
-        let trailerUrl = ''
-        for (let i = 0; i < movieTrailer.results.length; i++) {
-            movieTrailer.results[i].name == 'Official Trailer' || movieTrailer.results[i].name == 'Main Trailer' ? trailerUrl = `https://www.youtube.com/embed/${movieTrailer.results[i].key}?enablejsapi=1` : ''
-        }
-        trailerUrl == '' ? trailerUrl = `https://www.youtube.com/embed/${movieTrailer.results[0].key}?enablejsapi=1` : ''
-        let youtubeEmbed = youtubeTemplate.content.cloneNode(true);
-        youtubeEmbed.querySelector(`iframe`).setAttribute('src', trailerUrl);
-        trailer.appendChild(youtubeEmbed);
-
+        movie.trailer = await trailerResponse.json();
     } catch (error) {
         console.error(error);
     }
+    return movie;
+}
+
+async function showMovieDetails(btn) {
+    const youtubeTemplate = document.getElementById('youtube-video');
+    let movie = await getMovie(btn.getAttribute('data-movie-id'));
+    let movieModal = document.getElementById('movie-modal')
+
+    let title = movieModal.querySelector('.modal-title');
+    title.textContent = movie.details.title;
+
+    // header
+    let genres = movieModal.querySelector('.movie-genres');
+    let genresHtml = '';
+    for (let i = 0; i < movie.details.genres.length; i++) {
+        genresHtml += `<button class="btn btn-primary btn-sm px-1 py-0 mx-1">${movie.details.genres[i].name}</button>`
+    }
+    genres.innerHTML = genresHtml;
+
+    // left column
+    let poster = movieModal.querySelector('img');
+    poster.setAttribute('src', `https://image.tmdb.org/t/p/w500${movie.details.poster_path}`);
+
+    let release = movieModal.querySelector('.movie-release');
+    let releaseDateArry = `${movie.details.release_date}`.split('-');
+    let releaseDate = `${releaseDateArry[1]}/${releaseDateArry[2]}/${releaseDateArry[0]}`;
+    release.textContent = releaseDate;
+
+    let budget = movieModal.querySelector('.movie-budget');
+    let budgetFormat = Intl.NumberFormat('en-us', {
+        style: 'currency',
+        currency: 'USD',
+    });
+    budget.textContent = budgetFormat.format(movie.details.budget);
+    
+    // middle column
+    let tagline = movieModal.querySelector('.movie-tagline');
+    tagline.textContent = movie.details.tagline;
+
+    let director = movieModal.querySelector('.movie-director');
+    let directorHtml = ''
+    for (let i = 0; i < movie.credits.crew.length; i++) {
+        movie.credits.crew[i].job == 'Director' ? directorHtml += `${movie.credits.crew[i].name}` : '';
+    }
+    director.innerHTML = directorHtml;
+
+    let writer = movieModal.querySelector('.movie-writer');
+    let writerHtml = ''
+    for (let i = 0; i < movie.credits.crew.length; i++) {
+        movie.credits.crew[i].job == 'Screenplay' || movie.credits.crew[i].job == 'Writer' ? writerHtml += `${movie.credits.crew[i].name}` : '';
+    }
+    writer.innerHTML = writerHtml;
+
+    let stars = movieModal.querySelector('.movie-stars');
+    let starsHtml = '';
+    for (let i = 0; i < 4; i++) {
+        starsHtml += `${movie.credits.cast[i].name}, ${movie.credits.cast[i].character}`
+    }
+    stars.innerHTML = starsHtml;
+
+    // right column
+    let desc = movieModal.querySelector('.movie-description');
+    desc.textContent = movie.details.overview;
+
+    let trailer = movieModal.querySelector('.movie-trailer');
+    trailer.innerHTML = ''
+    let trailerUrl = ''
+    for (let i = 0; i < movie.trailer.results.length; i++) {
+        movie.trailer.results[i].name == 'Official Trailer' || movie.trailer.results[i].name == 'Main Trailer' ? trailerUrl = `https://www.youtube.com/embed/${movie.trailer.results[i].key}?enablejsapi=1` : ''
+    }
+    trailerUrl == '' ? trailerUrl = `https://www.youtube.com/embed/${movie.trailer.results[0].key}?enablejsapi=1` : ''
+    let youtubeEmbed = youtubeTemplate.content.cloneNode(true);
+    youtubeEmbed.querySelector(`iframe`).setAttribute('src', trailerUrl);
+    trailer.appendChild(youtubeEmbed);
 }
 
 function closePlayer() {
     let player = document.getElementById('movie-modal');
     player.querySelector('iframe').setAttribute('src', '');
-
-    // what *SHOULD HAVE* worked...
-    // let div = document.getElementById('movie-modal');
-    // let player = div.querySelector('.yt-player-iframe');
-    // player.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*')
-
-    // $('.yt-player-iframe')[0].contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*')
-
-    // player.foreach(() => {
-    //     this.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*')
-    // });
-
-    // $('.yt-player-iframe').each(function () {
-    //     // iframeElement.contentWindow.postMessage('{"event":"command","func":"pauseVideo","args":""}', '*')
-    //     this.contentWindow.postMessage('{"event":"command","func":"stopVideo","args":""}', '*')
-    // });
 }
